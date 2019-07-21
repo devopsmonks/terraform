3a6688ca646b1cf177a16c438399e8a2ad6d12bc
Arulelango Jayaraman <arul.elango@gmail.com>
2019-07-21 15:52:56 +0100

    merged with master

--- 1_variables/vars.tf
+++ 1_variables/vars.tf
@@ -3,7 +3,7 @@
 # variable "AWS_ACCESS_KEY" {}
 # variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "eu-east-1"
 }
 variable "AWS_PROFILE" {
   default = "terraform"
--- 3_vpc_nat/vars.tf
+++ 3_vpc_nat/vars.tf
@@ -7,9 +7,21 @@ variable "AWS_PROFILE" {
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
+<<<<<<< Updated upstream
   default = "../../.ssh/devopsmonks.pub"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
   default = "../../.ssh/devopsmonks.pem"
+=======
+  default = "~/.ssh/devopsmonks.pub"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/devopsmonks.pem"
+}
+
+variable "AWS_PROFILE" {
+  default = "terraform"
+>>>>>>> Stashed changes
 }
