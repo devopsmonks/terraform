cab43064e8853e78977d1b6ceb213ae49fa69a07
devopsmonksravi <devopsmonksravi@gmail.com>
2019-08-04 13:07:00 +0100

    changes to my 11 folder

--- 11_iam_role/vars.tf
+++ 11_iam_role/vars.tf
@@ -1,6 +1,9 @@
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
+variable "AWS_PROFILE" {
+  default = "terraform"
+}
   variable "PATH_TO_PUBLIC_KEY" {
   default = "../../.ssh/devopsmonks.pub"
 }
@@ -18,5 +21,5 @@ variable "AMIS" {
 }
 
 variable "MYBUCKETNAME" {
-  default = "devopsmonks"
+  default = "devopsmonks-ravi"
 }
